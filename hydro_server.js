73b1f0dece95a5fe8bd63e989767a0beb7654b01
mireles <mireles@horseless.io>
2021-09-30 14:45:51 -0400

    Moved from SQLite to MySQL database in hydro_server.js

--- hydro_server.js
+++ hydro_server.js
@@ -1,22 +1,35 @@
-const { readFileSync, writeFileSync } = require('fs');
-const sqlite3 = require('sqlite3').verbose()
+const { readFileSync, writeFileSync } = require('fs')
+const mysql = require('mysql')
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 let path = "/home/pi/iothydro"
 
-let db = new sqlite3.Database(path + "/sensor_readings.db")
+let db = mysql.createConnection({
+	host: "localhost",
+	user: process.env.username,
+	password: process.env.password,
+	database: "sensors",
+});
+
 let sql = "SELECT * FROM readings ORDER BY id DESC LIMIT 0,1";
 let timestamp = ''
 let temp = ''
 let hum = ''
 let distance = ''
-let result = db.get(sql, (err, row) => {
-	timestamp = row.timestamp
-	temp = row.temperature
-	hum = row.humidity
-	distance = row.distance
+let outputs = ''
+db.connect(function(err) {
+	if (err) throw err;
+
+	db.query(sql, function(err, result) {
+		if (err) throw err
+		outputs = result
+		timestamp = result[0].timestamp
+		temp = result[0].temperature
+		hum = result[0].humidity
+		distance = result[0].distance
+	})
 })
 
 app.get('/', (req, res) => {
